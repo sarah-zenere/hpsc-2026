44e62ced8a658b4a21fdc4dcf2d380f8be782317
sarah-zenere <132428074+sarah-zenere@users.noreply.github.com>
2026-04-29 23:46:25 +0900

    Update 11_distributed.cpp
    MPI_Put...manually edited because vscode won't commit it

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <mpi.h>
+//manually editing this bc vscode won't commit :(
 
 struct Body {
   double x, y, m, fx, fy;
@@ -9,45 +10,126 @@ struct Body {
 
 int main(int argc, char** argv) {
   const int N = 20;
+
   MPI_Init(&argc, &argv);
+
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
-  srand48(rank);
-  for(int i=0; i<N/size; i++) {
-    ibody[i].x = jbody[i].x = drand48();
-    ibody[i].y = jbody[i].y = drand48();
-    ibody[i].m = jbody[i].m = drand48();
-    ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
+
+  //change N
+  if (N % size != 0) {
+    if (rank == 0) {
+      printf("Error: N must be divisible by number of processes\n");
+    }
+    MPI_Finalize();
+    return 1;
   }
-  int recv_from = (rank + 1) % size;
-  int send_to = (rank - 1 + size) % size;
+
+  const int local_n = N / size;
+
+  Body ibody[local_n];     
+  Body jbody[local_n];     
+  Body send_buf[local_n];  //ring around the rosie
+
+  srand48(rank);
+
+  //init bodies
+  for (int i = 0; i < local_n; i++) {
+    ibody[i].x = drand48();
+    ibody[i].y = drand48();
+    ibody[i].m = drand48();
+
+    ibody[i].fx = 0.0;
+    ibody[i].fy = 0.0;
+
+    //ring 
+    jbody[i] = ibody[i];
+    send_buf[i] = ibody[i];
+  }
+
+  //MPI dt for Body
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
-  for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    for(int i=0; i<N/size; i++) {
-      for(int j=0; j<N/size; j++) {
+
+  //window exposing jbody
+  MPI_Win win;
+  MPI_Win_create(
+      jbody,
+      local_n * sizeof(Body),
+      sizeof(Body),
+      MPI_INFO_NULL,
+      MPI_COMM_WORLD,
+      &win
+  );
+
+  int next = (rank + 1) % size;
+
+  for (int irank = 0; irank < size; irank++) {
+
+    //jbody contents
+    for (int i = 0; i < local_n; i++) {
+      for (int j = 0; j < local_n; j++) {
+
         double rx = ibody[i].x - jbody[j].x;
         double ry = ibody[i].y - jbody[j].y;
+
         double r = std::sqrt(rx * rx + ry * ry);
+
         if (r > 1e-15) {
           ibody[i].fx -= rx * jbody[j].m / (r * r * r);
           ibody[i].fy -= ry * jbody[j].m / (r * r * r);
         }
       }
     }
-  }
-  for(int irank=0; irank<size; irank++) {
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(irank==rank) {
-      for(int i=0; i<N/size; i++) {
-        printf("%d %g %g\n",i+rank*N/size,ibody[i].fx,ibody[i].fy);
+
+   
+    if (irank < size - 1) {
+
+      //epoch
+      MPI_Win_fence(0, win);
+
+     
+      MPI_Put(
+          send_buf,     
+          local_n,         
+          MPI_BODY,       
+          next,           
+          0,              
+          local_n,         
+          MPI_BODY,       
+          win
+      );
+
+      //
+      MPI_Win_fence(0, win);
+
+      for (int i = 0; i < local_n; i++) {
+        send_buf[i] = jbody[i];
       }
     }
   }
+
+  //results in rank order
+  for (int irank = 0; irank < size; irank++) {
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (irank == rank) {
+      for (int i = 0; i < local_n; i++) {
+        printf("%d %g %g\n",
+               i + rank * local_n,
+               ibody[i].fx,
+               ibody[i].fy);
+      }
+    }
+  }
+
+  MPI_Win_free(&win);
+  MPI_Type_free(&MPI_BODY);
+
   MPI_Finalize();
-}
+
+  return 0;
+} //ssss
